d4f3a5e673882f85909adfc29b95a4f921d845cf
lootAssistant <lootassistant@gmail.com>
2017-11-25 12:30:10 +0000

    cache

--- antorusFights.js
+++ antorusFights.js
@@ -10,7 +10,28 @@ $( document ).ready(function() {
     console.log( "ready!" );
     startSizing();
     addMouseOvers();
+	cacheCheck()
 });
+
+function cacheCheck(){
+	// Check if a new cache is available on page load.
+	window.addEventListener('load', function(e) {
+
+	  window.applicationCache.addEventListener('updateready', function(e) {
+		if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+		  // Browser downloaded a new app cache.
+		  // Swap it in and reload the page to get the new hotness.
+		  window.applicationCache.swapCache();
+		  if (confirm('A new version of this site is available. Load it?')) {
+			window.location.reload();
+		  }
+		} else {
+		  // Manifest didn't changed. Nothing new to server.
+		}
+	  }, false);
+
+	}, false);
+}
 
 function startSizing(){
     for (i=0;i<sizes.length;i++){
